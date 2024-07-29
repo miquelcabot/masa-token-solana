c45d588522489000cc763212c6afed58ba7db600
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-29 12:26:05 +0200

    rename program from masa-token-solana to masa-token

--- tests/masa-token.ts
+++ tests/masa-token.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { MasaTokenSolana } from "../target/types/masa_token_solana";
 
-describe("masa-token-solana", () => {
+describe("masa-token", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
